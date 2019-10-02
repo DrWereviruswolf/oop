7cd4473022fdfc0ba7cea3ac3917e1f163b799e7
陆凌天 <1037192439@qq.com>
2019-10-02 23:57:39 +0800

    Lesson 4: Class and Object

---
+++ src/main/java/werewolf/github/com/oop/BookManager.java
@@ -0,0 +1,57 @@
+package werewolf.github.com.oop;
+
+class Book {
+    String name = "default";
+    long ID = 0;
+
+    public static final char STUD_OWN = 0;
+    public static final char MANA_OWN = 1;
+    char owner = MANA_OWN;
+    Book() {
+    }
+
+    Book(String newName, long newID){
+        name = newName;
+        ID = newID;
+    }
+    void Lended(){
+        owner = STUD_OWN;
+    }
+    void Returned(){
+        owner = MANA_OWN;
+    }
+}
+
+class Undergraduate {
+    String name = "default";
+    long ID = 0;
+    Undergraduate() {
+    }
+
+    Undergraduate(String newName, long newID){
+        name = newName;
+        ID = newID;
+    }
+}
+
+public class BookManager {
+    String name = "default";
+    long ID = 0;
+    BookManager() {
+    }
+
+    BookManager(String newName, long newID){
+        name = newName;
+        ID = newID;
+    }
+
+    long LendABook(Book book, Undergraduate student){
+        book.Lended();
+        return student.ID;
+    }
+
+    long RecieveABook(Book book, Undergraduate student){
+        book.Returned();
+        return student.ID;
+    }
+}
---
+++ src/main/java/werewolf/github/com/oop/Car.java
@@ -0,0 +1,17 @@
+package werewolf.github.com.oop;
+
+public class Car {
+    String name = "Default";
+    Car(){
+    }
+
+    Car(String newName){
+        name = newName;
+    }
+}
+
+class Factory{}
+
+class Owner{}
+
+class Manager{}
